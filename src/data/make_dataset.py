f30310b67c2c0d85f14ce0c627f643aa96daeb10
dpatroucheva <dpatr044@uottawa.ca>
2021-02-07 12:53:17 -0500

    Added raw tarball extraction when running script for the first time.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import shutil
+from os import listdir, mkdir, path
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
@@ -14,7 +16,12 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-
+    
+    if not path.exists('./data/external'):
+        logger.info('extracting raw data to data/external') 
+        mkdir('data/external')
+        for tar in listdir(input_filepath):
+            shutil.unpack_archive(input_filepath + '/' + tar, 'data/external')
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
