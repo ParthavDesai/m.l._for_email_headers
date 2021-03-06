cc2b97af37b41c6497b2c9db0790d0665b49c184
dpatroucheva <dpatr044@uottawa.ca>
2021-03-06 18:36:59 -0500

    Interim data generation optimization.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -10,20 +10,27 @@ from dotenv import find_dotenv, load_dotenv
 
 logger = logging.getLogger(__name__)
 columns = ['Return-Path', 'Message-ID', 'From', 'Reply-To', 'To', 'Subject', 'Date', 'X-Mailer', 'MIME-Version', 'Content-Type', 'X-Priority', 'X-MSMail-Priority', 'Status', 'Content-Length', 'Content-Transfer-Encoding', 'Lines', 'Label']
-df = pd.DataFrame(columns=columns)
 
-def addEmailToDf(file_path):
-    global df
+def getIndexMap(index_path, data_path):
     
-    index_path = 'data/raw/trec07p/full/index'
-    mail_id = file_path.split('.')[1]
-    header = dict.fromkeys(columns)
+    index = {}
 
-    label = 0
-    with open(index_path, encoding='us-ascii') as index:
-        for i, line in enumerate(index):
-            if i == int(mail_id) - 1 and line[0:4] == 'spam':
-                label = 1 
+    with open(index_path, encoding='us-ascii') as index_file:
+        for i, line in enumerate(index_file):
+            type = line[0:4]
+            file_path = line.split('/')[2][:-1]
+            if type == 'spam':
+                index[f'{data_path}{file_path}'] = 1
+            else:
+                index[f'{data_path}{file_path}'] = 0
+     
+    return index
+
+def addEmailToDf(file_path, index, df):
+    
+    header = dict.fromkeys(columns)
+    label = index[file_path]
+
     try:
         with open(file_path, encoding='us-ascii') as email:
             for line in email:
@@ -31,7 +38,23 @@ def addEmailToDf(file_path):
                 value = split_line[0]
                 if value in header:
                     header[value] = split_line[1]           
-            df.loc[-1] = [header['Return-Path'], header['Message-ID'], header['From'], header['Reply-To'], header['To'], header['Subject'], header['Date'], header['X-Mailer'], header['MIME-Version'], header['Content-Type'], header['X-Priority'], header['X-MSMail-Priority'], header['Status'], header['Content-Length'], header['Content-Transfer-Encoding'], header['Lines'], label]
+            df.loc[-1] = [header['Return-Path'], 
+                          header['Message-ID'], 
+                          header['From'], 
+                          header['Reply-To'], 
+                          header['To'], 
+                          header['Subject'], 
+                          header['Date'], 
+                          header['X-Mailer'], 
+                          header['MIME-Version'], 
+                          header['Content-Type'], 
+                          header['X-Priority'], 
+                          header['X-MSMail-Priority'], 
+                          header['Status'], 
+                          header['Content-Length'], 
+                          header['Content-Transfer-Encoding'], 
+                          header['Lines'], 
+                          label]
             df.index = df.index + 1
             df = df.sort_index()
     except UnicodeDecodeError:
@@ -44,16 +67,19 @@ def main(input_path, output_path):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
-    global df
     logger.info('making final data set from raw data')
-    
+
+    index_path = 'data/raw/trec07p/full/index'
+    index = getIndexMap(index_path, f'{input_path}/trec07p/data/')
     interim_path = 'data/interim'
+    df = pd.DataFrame(columns=columns)
+
     count = 0
     if not path.exists(interim_path):
         logger.info(f'converting external txt files to trec07.csv in {interim_path}')
         mkdir(interim_path)
         for email in listdir(f'{input_path}/trec07p/data'):
-            addEmailToDf(f'{input_path}/trec07p/data/{email}')
+            addEmailToDf(f'{input_path}/trec07p/data/{email}', index, df)
             count += 1
             if count % 1000 == 0:
                 logger.info(f'conversion done for {count}/75000 files')
